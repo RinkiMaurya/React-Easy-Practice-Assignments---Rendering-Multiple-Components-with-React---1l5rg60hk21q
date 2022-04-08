f8e17a49055b02594a4147f0184afef8b1654da0
RinkiMaurya <96375337+RinkiMaurya@users.noreply.github.com>
2022-04-08 18:06:11 +0530

    hii

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h2 value="project-name">Mahesh Kumar</h2>
+      <p value="project-description">
+        MERN Stack Developer | Full Stack Developer | Web Developer
+      </p>
+    </div>
+  );
+};
 
 export default App;
